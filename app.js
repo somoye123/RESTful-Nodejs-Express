5bf9e2338742868cc37794d82001d9d96d20ae7d
somoye123 <somoye.ayotunde@gmail.com>
2021-02-19 13:33:41 +0100

    refactor bookRouter to Router directory

--- app.js
+++ app.js
@@ -3,43 +3,14 @@ const mongoose = require('mongoose');
 
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const app = express();
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 // Add middlewares for parsing JSON and urlencoded data and populating `req.body`
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
-
-bookRouter
-  .route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    book.save();
-    res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-bookRouter.route('/books/:bookId').get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(book);
-  });
-});
 
 app.use('/api', bookRouter);
 
