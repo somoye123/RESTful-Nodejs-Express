86cdebb01aa0474f2ad20a111f2198d3d5026454
somoye123 <somoye.ayotunde@gmail.com>
2021-02-19 11:09:30 +0100

    add bookRouter to app

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 
 const app = express();
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
-app.get(
-  ('/',
-  (req, res) => {
-    res.send('welcome to my API');
-  }),
-);
+bookRouter.route('/books').get((req, res) => {
+  const response = { hello: 'this is my api' };
+  res.json(response);
+});
+
+app.use('/api', bookRouter);
+
+app.get('/', (req, res) => {
+  res.send('welcome to my API');
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
